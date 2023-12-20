597f6bdaec43d1b6d76861af8db1ee144a7a0bc5
Juan Gorosito <jpablo.gorosito87@gmail.com>
2023-12-20 15:43:33 -0300

    agregado de detalle

--- ecommerce-Gorosito-Figueroa/src/app/components/detail-product/detail-product.component.ts
+++ ecommerce-Gorosito-Figueroa/src/app/components/detail-product/detail-product.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HomeServiceService } from '../../services/home-service.service';
+import { ActivatedRoute } from '@angular/router';
+import { DetailService } from '../../services/detail.service';
 @Component({
   selector: 'app-detail-product',
   templateUrl: './detail-product.component.html',
-  styleUrl: './detail-product.component.css'
+  styleUrl: './detail-product.component.css',
 })
-export class DetailProductComponent {
+export class DetailProductComponent implements OnInit {
+  products: any [] = [];
+  productSelected: any = {};
+  constructor(public homeservice : HomeServiceService, private route: ActivatedRoute, public detailservice: DetailService) {}
 
+  ngOnInit(): void {
+    this.homeservice.getProducts().subscribe((res)=>{
+      this.products = res
+      const idURL = this.route.snapshot.params['id'];
+      const product = this.products.filter((p)=> p.id == idURL)[0]
+      console.log(product);
+      this.detailservice.getGetProductById(product.id).subscribe((res) => {
+        this.productSelected = res;
+      })
+    })
 }
+}
+  
--- ecommerce-Gorosito-Figueroa/src/app/components/home/home.component.ts
+++ ecommerce-Gorosito-Figueroa/src/app/components/home/home.component.ts
@@ -5,41 +5,50 @@ import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent implements OnInit{
-  constructor (public homseService : HomeServiceService, private route : ActivatedRoute){}
+export class HomeComponent implements OnInit {
+  constructor(
+    public homseService: HomeServiceService,
+    private route: ActivatedRoute
+  ) {}
 
-
-  products:any=[];
-  categories:any[]=[];
+  products: any = [];
+  categories: any[] = [];
   catURL: string | undefined;
 
   ngOnInit(): void {
-    this.route.params.subscribe(params => {
+    this.route.params.subscribe((params) => {
       this.catURL = params['category'] || undefined;
       this.listProducts();
     });
   }
 
-  listProducts(){
-    console.log(this.catURL)
+  listProducts() {
+    console.log(this.catURL);
     if (this.catURL) {
-      this.homseService.getAllCategories().subscribe((res)=>{
+      this.homseService.getAllCategories().subscribe((res) => {
         this.categories = res;
-        let catFilter = this.categories.filter(category => category.name === this.catURL)[0];
-        console.log(catFilter.id)
-        this.homseService.getAllProductsByCategory(catFilter.id).subscribe((res)=>{
-          this.products = res;
-          console.log(res)
-        })
-      })
+        let catFilter = this.categories.filter(
+          (category) => category.name === this.catURL
+        )[0];
+        console.log(catFilter.id);
+        this.homseService
+          .getAllProductsByCategory(catFilter.id)
+          .subscribe((res) => {
+            if (res.length == 0) {
+              alert('Esta categoria no tiene producto!');
+            } else {
+              this.products = res;
+              console.log(res);
+            }
+          });
+      });
     } else {
       this.homseService.getProducts().subscribe((res) => {
         console.log(res);
         this.products = res;
-      })
+      });
     }
   }
-
 }
